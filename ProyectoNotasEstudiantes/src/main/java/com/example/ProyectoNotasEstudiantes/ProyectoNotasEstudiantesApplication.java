320fd1eee685d33b78ba9cd2576b933af9374b38
LuisPineda <programadesdecero2020@gmail.com>
2025-01-31 17:30:56 -0500

    Creando repositorios

---
+++ ProyectoNotasEstudiantes/src/main/java/com/example/ProyectoNotasEstudiantes/ProyectoNotasEstudiantesApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoNotasEstudiantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoNotasEstudiantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoNotasEstudiantesApplication.class, args);
+	}
+
+}
